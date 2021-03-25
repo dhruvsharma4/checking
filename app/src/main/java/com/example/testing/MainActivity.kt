b597d6aca381ecfee0e859c58ee3211e0cf4fd10
dhruvsharma <28dhs01@gmail.com>
2021-03-25 19:27:03 +0530

    checking

---
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,132 @@
+package com.example.testing
+
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+
+class MainActivity : AppCompatActivity() {
+    var currentImageUrl: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadMeme()
+    }
+
+    private fun loadMeme() {
+        // Instantiate the RequestQueue.
+        progressBar.visibility = View.VISIBLE
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        // Request a Json response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null, { response ->
+                currentImageUrl = response.getString("url")
+                Glide.with(this).asBitmap().load(currentImageUrl).listener(object :
+                    RequestListener<Bitmap> {
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Bitmap>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+
+                        progressBar.visibility = View.GONE
+                        shareMeme.isEnabled = false
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        model: Any?,
+                        target: Target<Bitmap>,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility = View.GONE
+                        shareMeme.isEnabled = true
+                        shareMeme.setOnClickListener {
+                            downloadThenShare(resource)
+                        }
+                        return false
+                    }
+
+                }).into(imageView)
+            },
+            {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show()
+            })
+
+        // Add the request to the RequestQueue.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Hey, Checkout this amazing meme I got from reddit $currentImageUrl"
+        )
+        val chooser = Intent.createChooser(intent, "Share this meme using....")
+        startActivity(chooser)
+    }
+
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+
+    private fun downloadThenShare(bitmap: Bitmap) {
+        val fileName = "MemeIt-${System.currentTimeMillis()}.png"
+        val filePath = "${this.cacheDir}/$fileName"
+        download(bitmap, filePath) {
+            shareImage(this, File(filePath))
+        }
+    }
+
+    private fun download(bitmap: Bitmap, path: String, finishDownload: () -> Unit) {
+        val file = File(path)
+        FileOutputStream(file).use { output ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, output)
+            finishDownload.invoke()
+        }
+    }
+
+    private fun shareImage(context: Context, file: File) {
+        val sharingIntent = Intent(Intent.ACTION_SEND)
+        sharingIntent.type = "image/*"
+        val uri = FileProvider.getUriForFile(context, "$packageName.provider", file)
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri)
+
+        val intentChooser = Intent.createChooser(sharingIntent, "Share via")
+
+        val resInfoList =
+            packageManager.queryIntentActivities(intentChooser, PackageManager.MATCH_DEFAULT_ONLY)
+
+        for (resolveInfo in resInfoList) {
+            val packageName = resolveInfo.activityInfo.packageName
+            grantUriPermission(packageName, uri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION or
+                        Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+
+        context.startActivity(intentChooser)
+    }
+}
